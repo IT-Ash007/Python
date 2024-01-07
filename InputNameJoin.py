cb38dca9b8257b32976929e1524858a2ea7a666a
IT-Ash007 <114336326+IT-Ash007@users.noreply.github.com>
2024-01-07 19:48:29 +0530

    Create InputNameJoin.py

---
+++ InputNameJoin.py
@@ -0,0 +1,6 @@
+def my_name_function1(fname,lname):
+  print("Hello "+fname+ lname+"! You just delved into python.")
+  
+fname=input("Please enter your first name")
+lname=input("Please enter your last name")
+my_name_function1(fname,lname)
